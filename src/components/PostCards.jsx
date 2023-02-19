9cc8d59bfee412f9acb445794a116d0cf2ed53e4
zerodott <93039953+casperDev3@users.noreply.github.com>
2023-02-19 14:47:39 +0200

    finnaly lection

--- src/components/PostCards.jsx
+++ src/components/PostCards.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 
-const PostCards = ({post}) => {
-    const {title, body} = post;
+const PostCards = ({ post }) => {
+  const { title, body } = post;
   return (
     <div className="card m-3">
       <div className="card-body">
-        <h5 style={{color: "#f00", textTransform: "capitalize"}} className="card-title">{title}</h5>
+        {/* x btn */}
+        <div className="d-flex justify-content-end">
+          <button className="btn btn-danger btn-sm float-right o">x</button>
+        </div>
+        <h5
+          style={{ color: "#f00", textTransform: "capitalize" }}
+          className="card-title"
+        >
+          {title}
+        </h5>
         <p className="card-text">{body}</p>
       </div>
     </div>
--- src/pages/Student.jsx
+++ src/pages/Student.jsx
@@ -6,6 +6,9 @@ const Student = () => {
   //1. Create a state variable to store the posts
   let [posts, setPosts] = useState([]);
   let [loading, setLoading] = useState(false);
+
+  let [title, setTitle] = useState("");
+  let [body, setBody] = useState("");
   
   // On mount, fetch the posts
   useEffect(() => {
@@ -30,6 +33,16 @@ const Student = () => {
     //3. Set the posts state variable to the data
     setPosts(DATA);
   }
+  function addPost(e) {
+    e.preventDefault();
+    const newPost = {
+      id: posts.length + 1,
+      title: title,
+      body: body,
+      uid: 2
+    };
+    setPosts([newPost, ...posts]);
+  }
   return (
     <>
       <div className="container">
@@ -45,6 +58,19 @@ const Student = () => {
           </div>
         </div>
       </div>
+      <section>
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              <form>
+                <input className="form-label" onChange={(e)=>{setTitle(e.target.value)}} type="text" placeholder="Title" />
+                <textarea className="form-label" onChange={(e)=>{setBody(e.target.value)}} placeholder="Body" />
+                <button onClick={addPost} type="submit">Submit</button>
+              </form>
+              </div>
+          </div>
+        </div>
+      </section>
       <section>
         <div className="container">      
             <div className="row">
